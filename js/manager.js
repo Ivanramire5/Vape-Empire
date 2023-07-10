b0de27da260996aacc53c06cc4a553232e1bd427
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-07-10 15:43:32 -0300

    primera pre entrega

--- js/manager.js
+++
@@ -1,9 +0,0 @@
-import { ProductManager } from "./ProductManager";
-import { __dirname } from "./utils.js"
-
-let miPrimerStore = new ProductManager("./productosVapeo.json");
-miPrimerStore.getProducts().then((data) => console.log(data))
-
-miSegundaStore
-    .deleteProductById("d3068e07-b07b-49a6-b083-1d2b09f119aa")
-    .then((data) => console.log("El resultado eliminado es:", data));
